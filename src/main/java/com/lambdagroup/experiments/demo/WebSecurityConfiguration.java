11c5681da641261a5e6d9dede762cea5a32c1e86
ravikp <292991+ravikp@users.noreply.github.com>
2020-08-28 13:04:42 +0530

    Make application stateless

--- src/main/java/com/lambdagroup/experiments/demo/WebSecurityConfiguration.java
+++ src/main/java/com/lambdagroup/experiments/demo/WebSecurityConfiguration.java
@@ -3,9 +3,11 @@ package com.lambdagroup.experiments.demo;
 import lombok.val;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.SecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
 
 @Configuration
@@ -17,11 +19,15 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
         requestHeaderAuthFilter.setAuthenticationManager(getAuthenticationManager());
 
         http.csrf().disable()
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
             .authorizeRequests().antMatchers("/api/v1/**").permitAll()
             .anyRequest().denyAll()
             .and()
             .addFilter(requestHeaderAuthFilter)
         ;
+
     }
 
     private AuthenticationManager getAuthenticationManager() {
